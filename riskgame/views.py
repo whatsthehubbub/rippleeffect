5036f9e4723b8e9069bb866d574d22386a59ae60
Alper Çugun <github@alper.nl>
2013-03-15 17:30:17 +0100

    made login required homepage

--- riskgame/views.py
+++ riskgame/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.http import HttpResponse
+# from django.template import RequestContext, loader
+# from django.core.urlresolvers import reverse
+
+from django.contrib.auth.decorators import login_required
+
+@login_required
+def index(request):
+    # t = loader.get_template('boogie/index.html')
+    
+    # c = RequestContext(request, {
+    # })
+
+    return HttpResponse("Welcome to Ripple Effect")
