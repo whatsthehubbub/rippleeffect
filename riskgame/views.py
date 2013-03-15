995f89c203b4a876f409d4e02d030f02b3b87924
Alper Çugun <github@alper.nl>
2013-03-15 18:30:03 +0100

    added logout link

--- riskgame/views.py
+++ riskgame/views.py
@@ -1,14 +1,13 @@
 from django.http import HttpResponse
-# from django.template import RequestContext, loader
+from django.template import RequestContext, loader
 # from django.core.urlresolvers import reverse
 
 from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    # t = loader.get_template('boogie/index.html')
+    t = loader.get_template('index.html')
     
-    # c = RequestContext(request, {
-    # })
+    c = RequestContext(request, {})
 
-    return HttpResponse("Welcome to Ripple Effect")
+    return HttpResponse(t.render(c))
