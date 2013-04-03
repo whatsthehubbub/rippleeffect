2bf6d6ba657e321df7dab8d152c047ec04467198
Will Perkins <wilperkins@gmail.com>
2013-04-03 12:35:29 +0200

    freeze nginx version

--- fabfile/nginx.py
+++ fabfile/nginx.py
@@ -2,7 +2,7 @@ from fabric.api import *
 
 def install_nginx():
     "install nginx deb package"
-    sudo('apt-get install nginx --assume-yes')
+    sudo('apt-get install nginx=1.1.19-1ubuntu0.1 --assume-yes')
 
 def restart_nginx():
     "restart nginx if config is ok"
