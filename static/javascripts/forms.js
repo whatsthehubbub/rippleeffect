dcc835e02a564facae82d5940e612988d132d408
Bastiaan Terhorst <bastiaan@perceptor.nl>
2013-04-04 14:56:13 +0200

    finish up sign in page

---
+++ static/javascripts/forms.js
@@ -0,0 +1,11 @@
+(function() {
+
+  $(function() {
+    return $('.placeholder-container').each(function(i, el) {
+      var $el;
+      $el = $(el);
+      return $el.find(':nth-child(1)').watermark($el.data('placeholder'));
+    });
+  });
+
+}).call(this);
