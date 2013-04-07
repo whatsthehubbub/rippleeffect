a00ede01a717157703d6cab4c754dcfbce21c792
Will Perkins <wilperkins@gmail.com>
2013-04-07 17:58:23 +0200

    fixed indentation issue in manage

--- manage.py
+++ manage.py
@@ -6,13 +6,13 @@ if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "rippleeffect.settings")
 
     # read environment variables from .env file
-env_file = '.env'
-try:
-    env_vars = dict(line.strip('\n').split('=',1) for line in open(env_file,'rb'))
-    for name, val in env_vars.items():
-        os.environ.setdefault(name, val)
-except:
-    pass
+    env_file = '.env'
+    try:
+        env_vars = dict(line.strip('\n').split('=',1) for line in open(env_file,'rb'))
+        for name, val in env_vars.items():
+            os.environ.setdefault(name, val)
+    except:
+        pass
 
     from django.core.management import execute_from_command_line
 
