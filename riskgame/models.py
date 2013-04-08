bef1bc4711cd694790d2cd5d685964122cf6b02f
Alper Çugun <github@alper.nl>
2013-04-08 12:56:15 +0200

    reflow pumped values and add a decay to the piles

--- riskgame/models.py
+++ riskgame/models.py
@@ -388,10 +388,13 @@ class TeamPlayer(models.Model):
 
         pile = self.gather_pile.split(',')
         oil = 0 # Units of oil pumped
+        reflow = [] # What we are going to put back in the deck
 
         while gathersteps > 0:
             if pile:
                 output = pile.pop(0)
+
+                reflow.append(output)
 
                 if output == '1':
                     oil += 1
@@ -400,18 +403,22 @@ class TeamPlayer(models.Model):
 
             gathersteps -= 1
 
+        # We pump until everything is empty. Even if there are more markers than there are in the pile.
         self.gather_markers = 0
 
-        self.gather_pile = ','.join(pile)
+        self.gather_pile = ','.join(pile + reflow)
 
         # Now do the same with the risk pile
 
         pile = self.risk_pile.split(',')
         risks = 0
+        reflow = []
 
         while risksteps > 0:
             if pile:
                 output = pile.pop(0)
+
+                reflow.append(output)
 
                 if output == '1':
                     risks += 1
@@ -421,9 +428,11 @@ class TeamPlayer(models.Model):
 
             risksteps -= 1
 
-        self.risk_pile = ','.join(pile)
+        self.risk_pile = ','.join(pile + reflow)
 
-        # TODO add decay to both piles
+        # Both piles decay and are shuffled
+        self.put_and_discard('0', 'gather')
+        self.put_and_discard('1', 'risk')
 
         # If there are more risks than prevent markers, bad things will happen
         result = (oil, risks, self.prevent_markers)
