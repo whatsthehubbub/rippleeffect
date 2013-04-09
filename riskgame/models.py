f337391085097b00b76f8229ef44d935ca3004cc
Kars Alfrink <kars@leapfrog.nl>
2013-04-09 15:21:12 +0200

    Copy edit.

--- riskgame/models.py
+++ riskgame/models.py
@@ -252,9 +252,9 @@ class Notification(models.Model):
         elif self.identifier == 'player-retrieved-failure':
             return 'tried to retrieve resources but triggered an incident'
         elif self.identifier == 'player-gather':
-            return 'placed a gather'
+            return 'planned production'
         elif self.identifier == 'player-prevent':
-            return 'placed a barrier'
+            return 'erected a barrier'
 
     def get_subject(self):
         # TODO modify subjects based on notification type
