4924f1c86f3c384845be6a19920826e792e5d694
Bastiaan Terhorst <bastiaan@perceptor.nl>
2013-04-18 14:47:50 +0200

    sticky sidebar

--- static/coffeescripts/site.coffee
+++ static/coffeescripts/site.coffee
@@ -1,2 +1,13 @@
 $ ->
-  # console.log 'testing 123'
+  RE.initialize()
+
+@RE =
+
+  # hook up event handlers and intialize
+  initialize: ->
+    $(window).on('resize', -> RE.resize())
+    @resize()
+
+  # update sidebar height when viewport is resized
+  resize: ->
+    $('#sidebar').height($(window).height());
--- static/javascripts/site.js
+++ static/javascripts/site.js
@@ -1,5 +1,19 @@
 (function() {
 
-  $(function() {});
+  $(function() {
+    return RE.initialize();
+  });
+
+  this.RE = {
+    initialize: function() {
+      $(window).on('resize', function() {
+        return RE.resize();
+      });
+      return this.resize();
+    },
+    resize: function() {
+      return $('#sidebar').height($(window).height());
+    }
+  };
 
 }).call(this);
