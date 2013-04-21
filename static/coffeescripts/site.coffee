90fc7f9eb0eccc36fef9eec56d38d03fc53cc711
Bastiaan Terhorst <bastiaan@perceptor.nl>
2013-04-21 20:34:11 +0200

    hide the new game dialog by default (press 'd' to show it)

--- static/coffeescripts/site.coffee
+++ static/coffeescripts/site.coffee
@@ -6,8 +6,13 @@ $ ->
   # hook up event handlers and intialize
   initialize: ->
     $(window).on('resize', -> RE.resize())
+    $(window).on('keyup', RE.catchKey)
     @resize()
 
   # update sidebar height when viewport is resized
   resize: ->
     $('#sidebar').height($(window).height());
+
+  catchKey: (event) ->
+    if event.keyCode == 68
+      $('.debug').show();
--- static/javascripts/site.js
+++ static/javascripts/site.js
@@ -9,10 +9,16 @@
       $(window).on('resize', function() {
         return RE.resize();
       });
+      $(window).on('keyup', RE.catchKey);
       return this.resize();
     },
     resize: function() {
       return $('#sidebar').height($(window).height());
+    },
+    catchKey: function(event) {
+      if (event.keyCode === 68) {
+        return $('.debug').show();
+      }
     }
   };
 
