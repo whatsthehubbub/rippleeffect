18ffa557560ca68509c0e19a87c8c333b8208974
Alper Çugun <github@alper.nl>
2013-04-24 16:52:32 +0200

    force start time to be GMT

--- riskgame/models.py
+++ riskgame/models.py
@@ -15,6 +15,7 @@ import random
 import datetime
 import math
 import hashlib, hmac
+import pytz
 
 import logging
 logger = logging.getLogger('ripple')
@@ -951,6 +952,9 @@ class Game(models.Model):
 
         if not start:
             start = timezone.now()
+
+        GMT = pytz.timezone("GMT")
+        start.replace(tzinfo=GMT)
 
         self.start = start
 
