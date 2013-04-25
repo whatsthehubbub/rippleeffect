a0f39d6373d0d2d497088970d2de1a8afcd533f6
Bastiaan Terhorst <bastiaan@perceptor.nl>
2013-04-25 11:48:15 +0200

    load more messages through ajax

--- static/javascripts/site.js
+++ static/javascripts/site.js
@@ -12,6 +12,7 @@
       $(window).on('keyup', RE.catchKey);
       $(document).on('click', 'html:not(.touch) a:not(.js-no-refresh)', RE.handleButtonClick);
       $(document).on('click', 'html.touch a:not(.js-no-refresh)', RE.handleButtonTouch);
+      $(document).on('click', '#load-messages', RE.handleMoreMessages);
       return this.resize();
     },
     resize: function() {
@@ -26,6 +27,26 @@
     handleButtonTouch: function(e) {
       e.preventDefault();
       window.location.assign($(this).attr('href'));
+      return false;
+    },
+    handleMoreMessages: function(e) {
+      var currentPage, endpoint, lastPage, newPage;
+      e.preventDefault();
+      console.log('hier');
+      currentPage = $('#message-list').data('current-page');
+      lastPage = $('#message-list').data('last-page');
+      endpoint = $('#message-list').data('endpoint');
+      if (currentPage < lastPage) {
+        console.log('going');
+        newPage = currentPage + 1;
+        $.get(endpoint + newPage).done(function(data) {
+          $('#message-list').data('current-page', newPage);
+          if (newPage >= $('#message-list').data('last-page')) {
+            $('#load-messages-container').remove();
+          }
+          return $('#message-list-container').append(data);
+        });
+      }
       return false;
     }
   };
