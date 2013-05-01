c001902cc793edff979cf11c6bde3c20fbc95177
Bastiaan Terhorst <bastiaan@perceptor.nl>
2013-05-01 14:35:27 +0200

    attempt to create more variation in avatars

--- riskgame/templatetags/riskgame_extras.py
+++ riskgame/templatetags/riskgame_extras.py
@@ -25,7 +25,7 @@ def event_name(event_code):
 def team_color(team):
     # returns the calculated color for the team (derived from the team name) as a hex value
     if team and team.name:
-        return colorify(team.name, 0.1518, 0.8784)
+        return colorify(team.name + str(team.id) + "", 0.1518, 0.8784)
 
     return "#CFC59F"
 
@@ -34,7 +34,7 @@ def player_color(player):
     # returns the calculated color for the player (derived from the player's email address) as a hex value
     
     if player and player.email():
-        return colorify(player.email(), 0.3602, 0.6314)
+        return colorify(player.email() + str(player.user.id) + player.get_teamplayer().role, 0.3602, 0.6314)
     return "#CFC59F"
 
 @register.filter(name='times')
@@ -50,15 +50,17 @@ def colorify(str, s, v):
     m.update(str)
 
     # get a hue value by chopping the first two digits off a hex digest
-    hue = int(m.hexdigest()[:2], 16) # yields an 8-bit integer; 0-255
+    hue = int(m.hexdigest()[:4], 16) # yields an 8-bit integer; 0-255
+    #print "raw hue", str, hue
 
-    hue = hue / 255.0 # map 0-255 to 0-1 for colorsys
+    hue = hue / (255*255*1.0) # map 0-255 to 0-1 for colorsys
+    #print "normalized hue", str, hue
 
     # compute rgb values based on our new h value, and preset s and v values
     rgb = colorsys.hsv_to_rgb(hue, s, v)
 
     # scale the rgb 0-1 range back to 0-255
-    rgb = tuple([int(math.floor(255*x)) for x in rgb])
+    rgb = tuple([int(math.floor(255.0*x)) for x in rgb])
 
     # convert the rgb values to hex
     return '#%02x%02x%02x' % rgb
